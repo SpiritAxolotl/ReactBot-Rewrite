e66fc9d74e3d3b3e8d6fd9d4395260b7b8819f09
Spax <83354374+SpiritAxolotl@users.noreply.github.com>
2025-03-24 17:01:53 -0600

    he's gone again

--- src/main.js
+++ src/main.js
@@ -3,23 +3,51 @@ const core = tauri.core;
 const path = tauri.path;
 const fs = tauri.fs;
 
-const reactbot = document.querySelector(`#reactbot`);
-let appLocalDataDirPath = "";
-
-const getGrabBag = async (...paths) => {
-  return core.convertFileSrc(await path.join(appLocalDataDirPath, "GrabBag", ...paths));
-};
-
-const playRandomVideo = () => {
+const main = async () => {
+  const reactbot = document.querySelector(`#reactbot`);
+  let appLocalDataDirPath = "";
+  const [grabBagIdlePath, grabBagVideoPaths] = await (async () => {
+    const entries = await fs.readDir("GrabBag", { baseDir: fs.BaseDirectory.AppLocalData });
+    const filtered = entries.filter(e=>e.isFile).map(e=>e.name);
+    return [filtered.shift(), filtered];
+  })();
   
+  window.grabBagVideoPaths = grabBagVideoPaths;
+  window.grabBagIdlePath = grabBagIdlePath;
+  
+  const getGrabBagVideo = async (...paths) => {
+    return core.convertFileSrc(await path.join(appLocalDataDirPath, "GrabBag", ...paths));
+  };
+  
+  const playRandomVideo = async () => {
+    const rand = Math.floor(Math.random() * grabBagVideoPaths.length);
+    reactbot.removeAttribute("loop");
+    reactbot.src = await getGrabBagVideo(grabBagVideoPaths[rand]);
+    reactbot.currentTime = 0.0;
+    reactbot.play();
+  };
+  
+  const playIdleVideo = async () => {
+    reactbot.src = await getGrabBagVideo(grabBagIdlePath);
+    reactbot.setAttribute("loop");
+  };
+  
+  console.log("here");
+  
+  window.addEventListener("DOMContentLoaded", async () => {
+    console.log("here 3");
+    appLocalDataDirPath = await path.appLocalDataDir();
+    await playIdleVideo();
+    reactbot.addEventListener("ended", playIdleVideo);
+    window.addEventListener("keypress", (e) => {
+      e.preventDefault();
+      console.log(`key pressed: ${e.key}`);
+      if (e.key === "7") playRandomVideo();
+    });
+    console.log("here 4");
+  });
+  
+  console.log("here 2");
 };
 
-window.addEventListener("DOMContentLoaded", async () => {
-  appLocalDataDirPath = await path.appLocalDataDir();
-  reactbot.src = await getGrabBag("0 breathing idle loop.mp4");
-  window.addEventListener("keypress", (e) => {
-    e.preventDefault();
-    console.log(`key pressed: ${e.key}`);
-    if (e.key === "7") playRandomVideo();
-  });
-});
+main();
